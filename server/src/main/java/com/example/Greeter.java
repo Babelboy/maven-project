1752e8af0f02eadeac35278dadedf4e66ceb3471
Babelboy <114923491+Babelboy@users.noreply.github.com>
2022-10-05 15:14:20 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,4 +14,9 @@ public class Greeter {
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
+  
+  try 
+  { //nothing } 
+  catch (ExceptionInteressante e) {
+  }
 }
